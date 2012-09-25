8728ade490b9f1fa2b36c6ca48e2f8eca9c064f9
Kelly <kelly@kellyredding.com>
2012-09-25 13:38:33 -0500

    release script

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+RB_RELEASE='alpha1'
+
+# check uncommitted changes
+
+      if ! git diff-index --quiet HEAD --; then
+        echo "There are files that need to be committed first." && return 1
+      fi
+
+# tag the release
+
+      if git tag -a -m "Release $RB_RELEASE" "$RB_RELEASE"; then
+        echo "Tagged $RB_RELEASE release."
+
+# push the changes and tags
+
+        git push
+        git push --tags
+
+        echo "Pushed git commits and tags"
+      else
+        echo "Release aborted."
+      fi
+
+
